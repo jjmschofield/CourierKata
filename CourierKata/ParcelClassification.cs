b3062cc27c7eef02fbe131e00198e2ac69c70230
Jack Schofield <jjmschofield@googlemail.com>
2019-01-10 23:46:20 +0000

    Adding parcel classification enum

---
+++ CourierKata/ParcelClassification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourierKata
+{
+    public enum ParcelClassification
+    {
+        Small = 0,
+        Medium = 1,
+        Large = 2,
+        XL = 3,
+    }
+}
