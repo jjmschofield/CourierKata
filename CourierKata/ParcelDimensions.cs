945a2c24eb1df69352a7ae4691beb00203f0c146
Jack Schofield <jjmschofield@googlemail.com>
2019-01-10 23:50:13 +0000

    Adding a class to track parcel dimensions and conversions etc

---
+++ CourierKata/ParcelDimensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourierKata
+{
+    public class ParcelDimensions
+    {
+        public int WidthCm;
+        public int HeightCm;
+
+        public ParcelDimensions(int widthCm, int heightCm)
+        {
+            WidthCm = widthCm;
+            HeightCm = heightCm;
+        }
+    }
+}
