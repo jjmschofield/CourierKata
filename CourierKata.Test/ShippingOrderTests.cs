4d1f262ded18aed7bd9588f3ccd2fb9d33bb5c71
Jack Schofield <jjmschofield@googlemail.com>
2019-01-11 01:14:49 +0000

    Adding a shipping order class satisfying kata one

---
+++ CourierKata.Test/ShippingOrderTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace CourierKata.Test
+{
+    public class ShippingOrderTests
+    {
+        public class KataOne
+        {
+            Dictionary<int, double> PriceDictionary = new Dictionary<int, double>
+            {
+                { 0, 3 },
+                { 1, 8 },
+                { 2, 15 },
+                { 3, 25 }
+            };
+
+            [Fact]
+            public void Should_Correctly_Total_The_Price_Of_An_Order()
+            {
+                var expectedCost = 51.00;
+
+                var parcels = new List<Parcel>
+                {
+                    new Parcel(5, 9),
+                    new Parcel(11, 49),
+                    new Parcel(5, 51),
+                    new Parcel(101, 9),
+
+                };
+
+                var underTest = new ShippingOrder(parcels, PriceDictionary);
+
+                Assert.Equal(expectedCost, underTest.TotalCost);
+            }
+        }
+    }
+}
---
+++ CourierKata/ShippingOrder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourierKata
+{
+    public class ShippingOrder
+    {
+        public List<Parcel> Parcels { get; }
+        public double TotalCost { get; } = 0;
+        public string CurrencyCode { get; } = "USD";
+
+        public ShippingOrder(List<Parcel> parcels, Dictionary<int,double> priceByParcelType)
+        {
+            Parcels = parcels;
+
+            foreach (var parcel in parcels)
+            {
+                parcel.SetShippingCost(priceByParcelType);
+                TotalCost += parcel.ShippingCost;
+            }
+        }
+    }
+}
