c631d87b3e7facb00c342455773c327201071346
Jack Schofield <jjmschofield@googlemail.com>
2019-01-11 00:30:36 +0000

    Adding parcel class to hold relevent information about parcels

---
+++ CourierKata/Parcel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourierKata
+{
+    public class Parcel
+    {
+        public ParcelDimensions Dimensions { get; }
+        public ParcelClassification Type { get; }
+        
+
+        public Parcel(int widthCm, int heightCm)
+        {
+            Dimensions = new ParcelDimensions(widthCm, heightCm);
+            Type = new ParcelClassification(Dimensions);
+        }
+    }
+}
